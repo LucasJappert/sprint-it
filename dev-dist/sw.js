af719a68f15d64e4a8bb13af42638eb8cf0e5ae4
LucasJappert <lucasnicolasjappert@gmail.com>
2025-11-10 21:18:15 -0300

    feat(pwa): update manifest configuration and bump version to 1.1.9
    - Move PWA manifest generation from static file to vite.config.ts
    - Change theme color from #33c7ff to #05759eff
    - Add version query parameters to PWA assets for cache busting
    - Delete obsolete public/manifest.json file
    - Update service worker revision accordingly

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.trcnpjqpqcg"
+    "revision": "0.do7oi1drtjg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
